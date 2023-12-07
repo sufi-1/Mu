2878806612e19fd1aa04521e172102d5e205bef5
sufi-1 <133345424+sufi-1@users.noreply.github.com>
2023-12-07 22:17:28 +0300

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ if os.path.exists(".env"):
     load_dotenv(".env")
 # الفارات
 API_ID = 27703508
-API_HASH = 4cb84dd9cd3c3ee0bdb5c720fcfb7dba
+API_HASH = "4cb84dd9cd3c3ee0bdb5c720fcfb7dba"
 SESSION = "AgGmuNQAfE_nD0lmghrMjufYbhg7eaHSkTz6F3Kfhsx5kbdR4kbZygwP8fibvDa559FjnFHUlohFaqPU0GOdiNGr2ThkCUHDZPgVz9RZa8YKiv9GrsmgQqjudBRPRZ4mFFGP3qGN9dOo-B6bR8ODuolrQ2vOrwzK4bSyD9V2JhOAnRqCD9PnlaqlyrAagEUIq1Cs_roe_N2N-xTIMm_QT93_qBd_XnneZMa3uui4i4b5F35ZXdFME8YPp8c8TtzVXuOWlCAywYobhevwRGSbVT5tF2-yAmBRXMrebltn0GGzoaohBbhqaDGaebYNy-dXjtUZu4X0o6jl1DuqetJFfrs2UbLVQAAAAAGEUw3xAA"
 HNDLR = "."
 SUDO_USERS = 6515002865
